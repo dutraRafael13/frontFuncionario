08ac64a795f652f82a7db3d8c5f96c60a6b7f72b
Rafael Dutra <dutrarafaeloliveira@gmail.com>
2021-04-26 21:35:41 -0300

    Cria controlador do formulário e as validações de cada campo.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { IonicPage, NavController } from 'ionic-angular';
+import { FuncionarioService } from '../../services/domain/funcionario.service';
 
+@IonicPage()
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  formGroup: FormGroup;
 
+  constructor(
+    public navCtrl: NavController,
+    public funcionarioService: FuncionarioService,
+    public formBuilder: FormBuilder) {
+      this.formGroup = this.formBuilder.group({
+        nome: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(30)]],
+        sobrenome: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(50)]],
+        email: ['', [Validators.required, Validators.email]],
+        pis: ['', [Validators.required, Validators.minLength(11)]]
+      });
   }
 
 }
